085d14875bc67cf1f54cc26d11fc92125ff7c9d9
YuZakuro <zakuro@yuzakuro.me>
2015-03-18 17:52:54 +0900

    608

---
+++ SRM 608/OneDimensionalRobotEasy.cpp
@@ -0,0 +1,125 @@
+#include<bits/stdc++.h>
+#define INF (1 << 30)
+
+using namespace std;
+typedef long long ll;
+
+class OneDimensionalRobotEasy
+{
+public:
+  int finalPosition(string commands, int A, int B)
+  {
+    A = -A;
+    int p = 0;
+    for(int i = 0; i < commands.size(); i++)
+    {
+      if(commands[i] == 'L')
+        p--;
+      else
+        p++;
+      p = max(min(p, B), A);
+    } 
+    return p;
+  }
+};
+
+// CUT begin
+ifstream data("OneDimensionalRobotEasy.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(string commands, int A, int B, int __expected) {
+    time_t startClock = clock();
+    OneDimensionalRobotEasy *instance = new OneDimensionalRobotEasy();
+    int __result = instance->finalPosition(commands, A, B);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        string commands;
+        from_stream(commands);
+        int A;
+        from_stream(A);
+        int B;
+        from_stream(B);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(commands, A, B, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1426663196;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "OneDimensionalRobotEasy (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
